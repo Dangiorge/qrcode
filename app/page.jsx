dd872ebeb4f0642acde93184c4e1587900c87ee1
Dan - Giorge <dangiorge@gmail.com>
2026-01-04 23:00:05 +0300

    okay

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,6 @@ export default function GeneratePage() {
   const [qr, setQr] = useState("");
   const [loading, setLoading] = useState(false);
   const [copied, setCopied] = useState(false);
-  const link = "https://qrcode-mauve-psi.vercel.app/invite/undefined"
   // PIN MODAL STATES
   const [isPinModalOpen, setIsPinModalOpen] = useState(false);
   const [enteredPin, setEnteredPin] = useState("");
@@ -63,8 +62,9 @@ export default function GeneratePage() {
 
       const data = await res.json();
       setInvite(data);
+      const linkorigen = "https://qrcode-mauve-psi.vercel.app"
 
-      const link = `${link}/invite/${data.code}`;
+      const link = `${linkorigen}/invite/${data.code}`;
       const qrImg = await QRCode.toDataURL(link, { 
         width: 600,
         margin: 2,
